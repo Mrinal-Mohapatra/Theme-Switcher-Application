969829cdaeece5e1222f716442247443c830d183
Mrinal Mohapatra <49264474+Mrinal-Mohapatra@users.noreply.github.com>
2021-04-09 20:45:23 +0530

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ThemeToggler from "./ThemeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>theme Toggler</h1>
+      <ThemeToggler />
+    </header>
+  );
+};
+
+export default Header;
